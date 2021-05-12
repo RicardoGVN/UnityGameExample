573106e29e57f91214d51a0f62ec82591e08b576
RicardoGVN <ricardo.gomez@nerdino.com>
2021-05-11 22:23:55 -0500

    Guardado coloreado de cuarto, prototipo, colores, antes de dialogos

--- RPGBots/Assets/Scripts/ThirdPersonMover1.cs
+++ RPGBots/Assets/Scripts/ThirdPersonMover1.cs
@@ -7,12 +7,19 @@ public class ThirdPersonMover1 : MonoBehaviour
     [SerializeField] float _turnSpeed = 1000f;
     [SerializeField] float _moveSpeed = 5f;
     Rigidbody _rigidbody;
+    Animator _animator;
+
     /*// Start is called before the first frame update
-    void Start()
+        void Start()
     {
-        
-    }*/
-    private void Awake() => _rigidbody = GetComponent<Rigidbody>();
+       
+    }
+   */
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+    }
     
     // Update is called once per frame
     void Update()
@@ -24,12 +31,15 @@ public class ThirdPersonMover1 : MonoBehaviour
     void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
+        float vertical = Input.GetAxis("Vertical");//aquí da 1 o -1
+        if (Input.GetKey(KeyCode.LeftShift))
+            vertical *= 2f;
         var velocity = new Vector3(horizontal, 0, vertical);
         velocity *= _moveSpeed * Time.fixedDeltaTime;
         Vector3 offset = transform.rotation * velocity;
         _rigidbody.MovePosition(transform.position + offset);
-        
+
+        _animator.SetFloat("Horizontal", horizontal, 0.1f, Time.deltaTime);//entonces aquí va a cambiar el valor menor o mayor al que habíamos configurado
+        _animator.SetFloat("Vertical", vertical, 0.1f, Time.deltaTime);
     }
 }
